9fb2d14e1e7a0ad0f77cf40082fb7be1cbc14aca
sagarjaid <sagarjaid321@gmail.com>
2025-11-11 15:11:57 +0530

    facebook pixel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,15 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://aiforjr.com",
   generateRobotsTxt: true,
   // use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
-  exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*", "/data", "/data/**"],
+  exclude: [
+    "/twitter-image.*",
+    "/opengraph-image.*",
+    "/icon.*",
+    "/data",
+    "/data/**",
+    "/beta/",
+     "/beta/**",
+    "/apple-icon.png",
+    "/join",
+  ],
 };
